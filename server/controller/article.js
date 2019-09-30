60b4433311b8819ab5424f19df46ccba42924e84
curtainTan <1948750473@qq.com>
2019-09-30 21:44:28 +0800

    编写接口，待完善

--- server/controller/article.js
+++ server/controller/article.js
@@ -1,6 +1,9 @@
 const articleFun = require( "../model/article_fun" )
 const userFun = require( "../model/user_fun" )
 
+
+
+// 发表一篇文章
 const createArticle = async ctx => {
   if( ctx.state && ctx.state.token ){
     var data = ctx.request.body
@@ -16,6 +19,7 @@ const createArticle = async ctx => {
   }
 }
 
+// 上传文章的图片
 const uploadImage = async ctx => {
   ctx.body = {
     success: true,
@@ -23,6 +27,27 @@ const uploadImage = async ctx => {
   }
 }
 
+// 通过页码获取文章       按时间排序
+const getArticleByPage = async ctx => {
+
+}
+
+// 通过页码和标签获取文章
+const getArticleByPageAndLabel = async ctx => {
+
+}
+
+// 通过标题搜索文章
+const searchByTitle = async ctx => {
+
+}
+
+// 修改文章
+const modificationArticle = async ctx => {
+
+}
+
+// 
 
 module.exports = {
   createArticle,
